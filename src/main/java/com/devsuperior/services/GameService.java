9f9fa52d0a4d24bfafecd7ca063a289daa970856
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-11-12 16:43:17 -0300

    GET game by id

--- src/main/java/com/devsuperior/services/GameService.java
+++ src/main/java/com/devsuperior/services/GameService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dto.GameDTO;
 import com.devsuperior.dto.GameMinDTO;
+import com.devsuperior.entities.Game;
 import com.devsuperior.repositories.GameRepository;
 
 @Service
@@ -14,6 +17,13 @@ public class GameService {
 	@Autowired
 	GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		var result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
